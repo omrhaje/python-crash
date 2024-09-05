5477a3d2376a9f1b1ea12b5abcc4004cf172c6f8
omrhaje <omrhajje@gmail.com>
2024-09-05 23:20:42 +0200

    For loop
    • Execute a block of code in fixed number of times

---
+++ 17.For-Loops.py
@@ -0,0 +1,11 @@
+# for loops = execute a block of code a fixed number of times.
+#.            You can iterate over a range, string, sequence, etc.
+
+# break <-- will stop the code
+# continue <-- will skip the code you selected
+
+for x in range(1, 21):
+    if x == 13:
+        continue
+    else:
+        print(x)
