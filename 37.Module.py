b728b5abc5959a1d58c4f4a6823456d586f222c1
omrhaje <omrhajje@gmail.com>
2024-09-18 22:05:39 +0200

    Modules
    - A file containing code you want to include in your program
    
    - Use 'import' to include a module (built-in or your own)
    
    - Useful to break up a large program reusable separate files
    
    - Put your module in a file then call it from another file using the 'import' function

--- 37.Module.py
+++ 37.Module.py
@@ -1,6 +1,6 @@
 # ----------------- main.py -----------------
 # module = a file containing code you want to include in your program
-#          use 'import' to include a module (built_-in or your own)
+#          use 'import' to include a module (built-in or your own)
 #          useful to break up a large program reusable separate files
 
 print(help("modules"))
@@ -9,7 +9,7 @@ pi = 3.14159            # put your module in a file then call it from another fi
 
 def square(x):
     return x ** 2
-#                                   this code is used for being called from other files
+
 def cube(x):
     return x ** 3
 
