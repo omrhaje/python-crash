120954663b324619641a4eef15557ee26778cf9d
omrhaje <omrhajje@gmail.com>
2024-09-23 21:40:24 +0200

    Hangman Game
    – Word Selection:
    It picks a random word from a list (words) that you imported.
    
    – Hangman Drawing:
    You have a dictionary (hangman_art) that shows different stages of the hangman drawing as the player guesses wrong letters.
    
    – Main Functions:
    •  display_man(): Shows the current hangman drawing.
    •  display_hint(): Displays the word with underscores for missing letters.
    •  display_answer(): Shows the full word at the end.
    
    – Game Flow:
    •  The player guesses letters one by one.
    
    •  If they guess right, it fills in the blanks in the word. If they guess wrong, the hangman drawing progresses.
    
    •  The game ends when they either guess the word or make too many mistakes.
    
    – Endgame:
     If all letters are guessed correctly, the player wins. If they reach the max number of wrong guesses, they lose.

---
+++ wordslist.py
@@ -0,0 +1,25 @@
+# Words for hangman game
+
+words = ("aardvark", "alligator", "alpaca", "ant", "anteater" , "antelope" "ape", "armadillo", "baboon", "badger" ,"bat",
+"bear", "beaver", "bee" , "bison", "boar", "buffalo", "butterfly", "camel", "capybara", "caribou", "cat",
+"caterpillar", "cattle", "chamois", "cheetah", "chicken", "chimpanzee"
+"chinchilla" , "clam", "cobra", "cockroach", "cod" , "coyote" ,
+"crab", "crane" , "crocodile" , "crow", "curlew", "deer", "dinosaur", "dog"
+"dogfish", "dolphin", "donkey", "dormouse", "ottered", "dove", "dragonfly",
+"duck", "dugong", "dunlin", "eagle", "echidna", "eel", "eland", "elephant",
+"elephant-seal" , "elk", "emu", "falcon", "ferret", "finch", "fish", "flamingo",
+"fly", "fox", "frog", "gaur", "gazelle", "gerbil", "giant-panda", "giraffe",
+"gnat", "gnu", "goat", "goldfinch", "goldfish", "goose", "gorilla", "goshawk", "grasshopper",
+"grouse", "guanaco", "gull", "hamster", "hare", "hawk", "hedgehog",
+"heron", "herring", "hippopotamus" , "hornet", "horse", "human" ,"hummingbird"
+"hyena", "ibex", "ibis", "jackal", "jaguar", "jay", "jellyfish", "kangaroo"
+"kingfisher", "koala", "komodo-dragon", "kookaburra", "groupref", "kudu", "lapwing",
+"lark" ,"lemur", "leopard", "lion", "llama", "lobster", "locust" , "loris", "louse"
+"lyrebird", "magpie", "mallard", "manatee", "mandrill", "mantis" , "marten" , "meerkat"
+"mink", "mole", "mongoose", "monkey", "moose", "mosquito", "mouse", "mule", "narwhal",
+"newt", "nightingale", "octopus", "okapi", "opossum", "oryx", "ostrich", "otter", "owl",
+"ox", "oyster", "panther", "parrot", "partridge", "peafowl", "pelican", "penguin", "pheasant"
+"pig", "pigeon", "polar-bear", "pony", "porcupine", "porpoise", "quail", "quelea", "quetzal", "rabbit",
+"raccoon", "rail", "ram", "rat", "raven", "red-deer", "red-panda", "reindeer", "rhinoceros", "rook",
+"salamander", "salmon", "sand-dollar", "sandpiper", "sardine" , "scorpion", "seahorse", "seal", "shark",
+"sheep", "shrew", "skunk","snail")
