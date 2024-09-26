34022d8887f68d3d9d105dc5f9e4afdd93eeaca5
omrhaje <omrhajje@gmail.com>
2024-09-26 21:01:49 +0200

    Duck Typing
    1.Duck Typing:
    • If an object has the right methods (like speak()), it can be treated the same as other objects, even if they aren’t related through inheritance.
    
    2.Animal Class:
    • Sets an alive attribute (True for animals).
    
    3.Dog and Cat Classes:
    • Both inherit from Animal and have their own speak() methods (Dog says “WOOF!” and Cat says “Meow!”).
    
    4.Car Class:
    • It isn’t an animal but has the alive attribute (False) and a speak() method (says “HONK!”). So it can be treated like the animals in the list.
    
    5.Animals List:
    • The list includes a Dog, Cat, and Car.
    • The loop calls speak() and prints alive for each one, showing that even though Car isn’t an animal, it behaves like one because it has the necessary methods.

---
+++ 49.Duck-Typing.py
@@ -0,0 +1,28 @@
+# "Duck Typing" = Another way to achieve polymorphism besides Inheritance
+#                 Object must have the minimum necessary attributes/methods
+#                 "If it looks like a duck and quacks like a duck, it must be a duck."
+
+
+class Animal:
+    alive = True
+
+class Dog(Animal):
+    def speak(self):
+        print("WOOF!")
+
+class Cat(Animal):
+    def speak(self):
+        print("Meow!")
+
+class Car:                  # it has the minimum necessary methods to be considered an animal
+
+    alive = False
+
+    def speak(self):
+        print("HONK!")
+
+animals = [Dog(), Cat(), Car()]
+
+for animal in animals:
+    animal.speak()
+    print(animal.alive)
