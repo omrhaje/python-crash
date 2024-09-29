3abdca9abc0aa793c05cc3d7f127f51ec0fd2a8d
omrhaje <omrhajje@gmail.com>
2024-09-29 23:14:31 +0200

    Class Methods
    1.Student Class:
    •	It tracks students and their GPAs. The class keeps a count of how many students exist and the total GPA of all students.
    
    2.	Instance Method (get_info):
    •	It belongs to a specific student object and returns the student’s name and GPA. You would call it on individual student objects, like student1.get_info().
    
    3.Class Method (get_count):
    •	This method belongs to the class itself, not individual students. It returns how many students have been created by accessing the class variable count.
    
    4.Class Method (get_average_gpa):
    •	This also belongs to the class and calculates the average GPA by dividing total_gpa by the number of students (count). It gives the average GPA for all students created so far.
    
    5.Creating Students:
    •	Three students are created: Spongebob (GPA 2.9), Squidward (GPA 3.5), and Patrick (GPA 4.0).
    
    6.Output:
    •	The total number of students is printed (get_count()).
    •	The average GPA across all students is calculated and printed (get_average_gpa()).

---
+++ 51.Class-Methods.py
@@ -0,0 +1,40 @@
+# Class methods = Allow operations related to the class itself
+#                 Take (cls) as the first parameter, which represents the class itself
+
+
+# Instance methods = Best for operations on instances of the class (objects)
+# Static methods = Best for utility functions that do not need access to class data
+# Class methods = Best for class-level data or require access to the class itself
+
+class Student:
+
+    count = 0
+    total_gpa = 0
+
+    def __init__(self, name, gpa):
+        self.name = name
+        self.gpa = gpa
+        Student.count += 1
+        Student.total_gpa += gpa
+
+    # INSTANCE METHOD
+    def get_info(self):
+        return f"{self.name} = {self.gpa}"
+
+    @classmethod
+    def get_count(cls):
+        return f"Total # of students: {cls.count}"
+
+    @classmethod
+    def get_average_gpa(cls):
+        if cls.count == 0:
+            return 0
+        else:
+            return f"The average gpa is: {cls.total_gpa / cls.count: .2f}"
+
+student1 = Student("Spongebob", 2.9)
+student2 = Student("Squidward", 3.5)
+student3 = Student("Patrick", 4.0)
+
+print(Student.get_count())
+print(Student.get_average_gpa())
