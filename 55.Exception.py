f0374c6112cf5f34aee780a7bde3b58f6e15721f
omrhaje <omrhajje@gmail.com>
2024-10-03 21:06:46 +0200

    Exceptions
    1.	Exception Handling: This code is designed to handle errors (exceptions) that might happen during execution, like dividing by zero or entering an invalid input.
    
    2.	try Block:
    •  It attempts to convert user input to an integer and then divides 1 by that number.
    
    3.	except Blocks:
    •  ZeroDivisionError: If the user enters 0, it prints a message saying you can’t divide by zero.
    •  ValueError: If the input isn’t a number, it prints a message asking for numbers only.
    •  Exception: Catches any other unexpected error and prints a generic error message.
    
    4.	finally Block:
    •  Runs no matter what happens, used for cleanup tasks (like closing files or resetting variables).
    
    – It ensures the program doesn’t crash when something goes wrong

---
+++ 55.Exception.py
@@ -0,0 +1,16 @@
+# exception = An event that interrupts the flow of a program
+#             (ZeroDivisionError, TypeError, ValueError)
+#             1.try, 2.except, 3.finally
+from multiprocessing.managers import Value
+
+try:
+    number = int(input("Enter a number: "))
+    print(1 / number)
+except ZeroDivisionError:
+    print("you can't divide by zero")
+except ValueError:
+    print("Enter only numbers!")
+except Exception:
+    print("Something went wrong")
+finally:
+    print("Do some clean up here")
